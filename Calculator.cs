63f87095ec13f3d4ba3e6912412997ec8312263c
rahmat48 <rahmatdw48@gmail.com>
2020-03-11 14:54:39 +0700

    menambahkan fungsi penjumlahan

--- Calculator.cs
+++ Calculator.cs
@@ -2,6 +2,10 @@ class Calculator
 {
 	static void Main (string[] args)
 	{
-	
+		Console.WriteLine("Hasil Penambahan " + a + " +" + b + " = " + Penambahan(a, b));
 	}
+	static int Penambahan(int a,int b)
+        {
+            return a + b;
+        }
 }
